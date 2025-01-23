81d91c572bdfc30915cd030fcbbbfbe5fb86f503
Ahmed Imtiaz Khan <Ahmed.imtiaz2000@gmail.com>
2025-01-23 06:00:00 +0500

    added utils

--- src/App.jsx
+++ src/App.jsx
@@ -13,6 +13,7 @@ function App() {
   const [output, setOutput] = useState(true);
   const [loading, setLoading] = useState(false);
   const [finished, setFinished] = useState(false);
+  const [DOWNLOADING, setDOWNLOADING] = useState(false);
 
   const isAudioAvailable = file || audioStream;
   function handleAudioReset() {
@@ -24,10 +25,60 @@ function App() {
 
   useEffect(() => {
     if (!worker.current) {
-      worker.current = new Worker(new URL("./"));
+      worker.current = new Worker(
+        new URL("./utils/worker.js", import.meta.url),
+        { type: "module" }
+      );
     }
+    const onMessageReceived = async (e) => {
+      switch (e.data.type) {
+        case "DOWNLOADING":
+          setDOWNLOADING(true);
+          console.log("DOWNLOADING");
+          break;
+        case "LOADING":
+          setLoading(true);
+          console.log("LOADING");
+          break;
+        case "RESULT":
+          setOutput(e.data.payload);
+          console.log("RESULT");
+          break;
+        case "INFERENCE_DONE":
+          setFinished(true);
+          console.log("INFERENCE_DONE");
+          break;
+      }
+    };
+    worker.current.addEventListener("message", onMessageReceived);
+    onMessageReceived({ data: { type: "LOADING" } });
+    return () => {
+      worker.current.removeEventListener("message", onMessageReceived);
+    };
   }, []);
 
+  async function readAudioFile(file) {
+    const sampling_rate = 16000;
+    const audioCTX = new AudioContext({ sampleRate: sampling_rate });
+    const response = await file.arrayBuffer();
+    const decoded = await audioCTX.decodeAudioData(response);
+    const audio = decoded.getChannelData(0);
+    return audio;
+  }
+
+  async function handleFormSubmission() {
+    if (!file && !audioStream) {
+      return;
+    }
+    let audio = await readAudioFile(file ? file : audioStream);
+    const model_name = "openai/whisper-tiny.en";
+
+    worker.current.postMessage({
+      type: MessageTypes.INFERENCE_REQUEST,
+      audio,
+      model_name,
+    });
+  }
   return (
     <>
       <div className="flex flex-col p-4 max-w-[1000px] mx-auto w-full">
