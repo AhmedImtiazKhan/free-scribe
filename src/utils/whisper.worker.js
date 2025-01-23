d5fd0b3c86ef42aac73718322ceddc7671563635
Ahmed Imtiaz Khan <Ahmed.imtiaz2000@gmail.com>
2025-01-23 20:53:55 +0500

    using xenova

---
+++ src/utils/whisper.worker.js
@@ -0,0 +1,16 @@
+import {pipeline} from '@xenova/transformers';
+
+class MyTranscriptionPipeline{
+    static task = 'automatic-speech-recognition';
+    static model = 'openai/whisper-tiny.en';
+    static instance = null;
+
+    static async getInstance(progress_callback = null){
+        if(this.instance === null){
+            this.instance = await pipeline(this.task,null,null,{
+                progress_callback
+            });
+        }
+        return this.instance;
+    }
+}
