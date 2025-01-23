ea0cef315fd49b39e383d02ba19d7b01a3b60651
Ahmed Imtiaz Khan <Ahmed.imtiaz2000@gmail.com>
2025-01-24 04:00:57 +0500

    adding the transcription pipeline

--- src/utils/whisper.worker.js
+++ src/utils/whisper.worker.js
@@ -14,3 +14,31 @@ class MyTranscriptionPipeline{
         return this.instance;
     }
 }
+
+self.addEventListener('message',async (e) => {
+    const {type,audio} = e.data
+    if(type===MessageTypes.INFERENCE_REQUEST){
+        await transcribe(audio);
+    }});
+
+    async function transcribe(audio){
+        sendLoadingMessage('Loading');
+        let pipeline
+
+        try{
+            pipeline = await MyTranscriptionPipeline.getInstance(load_modal_callback);
+        }
+        catch(err){
+            console.log(err.message);
+        }
+
+        sendLoadingMessage('Success');
+
+        const stride_length_s=5;
+
+        const generationTracker= new GenerationTracker(pipeline,stride_length_s);
+        await pipeline(audio,{
+            top_k:0,
+            stride_length_s,
+        }
+    };
